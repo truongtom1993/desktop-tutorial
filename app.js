04f08dd1ee25eb7b8df18eccc767639b415ac022
Đặng Nhật Trường <truongdn@rikkeisoft.com>
2021-09-06 09:35:28 +0700

    Node1 Chuong2 Bai 3.5 Setup EJS Layout ap dung cho toan bo phan template, su dung express-ejs-layouts

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+const expressEjsLayouts = require('express-ejs-layouts');
+
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(expressEjsLayouts);
+app.set('layout', 'layout');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
